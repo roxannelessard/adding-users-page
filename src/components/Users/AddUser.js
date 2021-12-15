778ce8fcaf1cbf6f94532b4ed738897751819f0b
roxanne lessard <me@rlessard.com>
2021-12-14 21:13:51 -0500

    fix: adds validations and onChange on AddUser component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,41 @@
+import React, { useState } from 'react';
 import { Card }  from '../UI/Card';
 import classes from './AddUser.module.css'
 import { Button } from '../UI/Button'
 
 export const AddUser = (props) => {
 
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || setEnteredAge.trim().length === 0) {
+      return;
+    }
+    // by adding a + before enteredAge we ensure that we compare numbers
+    if (+enteredAge <  1) {
+      return;
+    }
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
         <Button type="submit">Add user</Button>
       </form>
     </Card>
